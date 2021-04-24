9f2e62b5835091ad4ce1542fffa55f7b3f536584
Jesus <valladaresleonardo2020@gmail.com>
2021-04-24 09:24:21 -0400

    Initial commit

---
+++ js/funciones.js
@@ -0,0 +1,23 @@
+window.onload=boton;
+function boton(){
+    document.querySelector("#accion").onclick=operacionraiz;
+}
+var Raiz = 0;
+var A = 1, B = 1, I = 1;
+function operacionraiz(){
+    Raiz = prompt("Dame el numero de la raiz cuadrada que desea calcular: ");
+    
+    for(I = 1; I <= Raiz; I++){
+        A++;
+        B++;
+
+        I = A * B;
+    }
+   
+        alert("El resultado Estimado de la raiz fue: " + A);  
+    }
+
+
+
+    
+
